583fed286b4007cca4bd2d535828b9680601b8e4
Jeff Melton <melman74@yahoo.com>
2023-06-06 20:53:32 -0500

    Ready to Submit

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ let grade3 = 65;
 let grade4 = 74;
 let passingGrade = 80;
 //1.Create a variable called average that computes for the average from grade1 to grade4
-
+let average = (grade1 + grade2 + grade3 + grade4) / 4;
 //2. Change the value of studentLastName to "Newman" 
-
+studentLastName = "Newman";
 //3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
 //ex. Jane Doe has an average grade of 100.
-
+console.log(`${studentFirstName} ${studentLastName} has an average grade of ${average}.`);
 //4. Write code that would answer this question: Did the student pass?
+console.log(`Did ${studentFirstName} ${studentLastName} pass (by scoring ${passingGrade} or better)? ${average >= passingGrade}`);
